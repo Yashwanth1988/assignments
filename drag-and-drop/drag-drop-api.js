cc9f49f9c6a9534cd940485599fbccdbae13104e
Yashwanth <bathiniyashwanth@gmail.com>
2020-04-15 19:36:51 +0530

    few fixes and enhancements

--- drag-and-drop/drag-drop-api.js
+++ drag-and-drop/drag-drop-api.js
@@ -15,50 +15,34 @@ function DragDropApi(){
 	}
 
 	this.onDragStop = function(){
-		var c = document.querySelector('#container')
-		c.dispatchEvent(new Event('mouseup', {bubbles: true}))
+		var containerElement = document.querySelector('#container')
+
+		var { left, right, top, bottom } = getCoordinates(containerElement)
+
+		if((event.clientX < left || event.clientX > right)
+				|| (event.clientY < top || event.clientY > bottom)
+			){
+			this.currentElement.style.position = 'absolute'
+			this.currentElement.style.left = left
+			this.currentElement.style.top = top
+			containerElement.dispatchEvent(new Event('mouseup', {bubbles: true}))
+		}
+
+		containerElement.dispatchEvent(new Event('mouseup', {bubbles: true}))
 	}
 
 	function moveTo(element){
 		element.style.cursor = 'move'
 		element.style.position = 'absolute'
-		element.style.left = event.clientX - element.offsetWidth/2 + 'px'
-		element.style.top = event.clientY + (element.offsetHeight/2)*2 + 'px'
+		element.style.left = event.clientX - element.offsetWidth + 6 + 'px'
+		element.style.top = event.clientY - element.offsetHeight + 6 + 'px'
 	}
 
-	this.onDrop = function(){
+	this.onDrop = function(event){
 		this.dragStarted = false
 	}
 
-	// this.onDrop = function(event){
-
-	// 	// on mouse release
-	// 	// detect draggable element is in the container
-	// 	// set the position of the draggable element to the current cursor position
-
-	// 	this.dragStarted = false
-	// 	var containerElement = document.querySelector(`#${event.target.id}`)
-
-	// 	if(!containerElement.style.position){
-	// 		containerElement.style.position = 'relative'
-	// 		containerElement.style.left = 0
-	// 		containerElement.style.top = 0
-	// 	}
-
-	// 	var { left, right, top, bottom } = getCoordinates(containerElement)
-
-	// 	if((event.clientX < left || event.clientX > right)
-	// 			|| (event.clientY < top || event.clientY > bottom)
-	// 		){
-	// 		this.currentElement.style.position = 'relative'
-	// 		this.currentElement.style.left = containerElement.style.left
-	// 		this.currentElement.style.top = containerElement.style.top
-	// 	}
-
-		
-	// }
-
-	// function getCoordinates(containerDOM){
-	// 	return containerDOM.getBoundingClientRect()
-	// }
+	function getCoordinates(containerDOM){
+		return containerDOM.getBoundingClientRect()
+	}
 }
