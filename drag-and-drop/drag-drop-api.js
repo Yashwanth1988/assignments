e5afe03fe0b29f713e25ffa3f71ac294362d04d7
Yashwanth <bathiniyashwanth@gmail.com>
2020-04-15 16:28:16 +0530

    handling multiple draggable elements - yet needs bug fixes

--- drag-and-drop/drag-drop-api.js
+++ drag-and-drop/drag-drop-api.js
@@ -1,38 +1,64 @@
 function DragDropApi(){
+	
+	this.dragStarted = false
+	this.currentElement = null
 
-	this.onDrag = function(event, element){
+	this.onDragStart = function(event){
+		this.dragStarted = true
+		this.currentElement = document.querySelector(`#${event.target.id}`)
+	}
+
+	this.onDrag = function(){
+		if(this.dragStarted){
+			moveTo(this.currentElement)
+		}
+	}
+
+	this.onDragStop = function(){
+		var c = document.querySelector('#container')
+		c.dispatchEvent(new Event('mouseup', {bubbles: true}))
+	}
+
+	function moveTo(element){
 		element.style.cursor = 'move'
 		element.style.position = 'absolute'
 		element.style.left = event.clientX - element.offsetWidth/2 + 'px'
 		element.style.top = event.clientY + (element.offsetHeight/2)*2 + 'px'
 	}
 
-	this.onDrop = function(event, element, container){
+	this.onDrop = function(){
+		this.dragStarted = false
+	}
 
-		// on mouse release
-		// detect draggable element is in the container
-		// set the position of the draggable element to the current cursor position
+	// this.onDrop = function(event){
 
-		if(!container.style.position){
-			container.style.position = 'relative'
-			container.style.left = 0
-			container.style.top = 0
-		}
+	// 	// on mouse release
+	// 	// detect draggable element is in the container
+	// 	// set the position of the draggable element to the current cursor position
 
-		var { left, right, top, bottom } = getCoordinates(container)
+	// 	this.dragStarted = false
+	// 	var containerElement = document.querySelector(`#${event.target.id}`)
 
-		if((event.clientX < left || event.clientX > right)
-				|| (event.clientY < top || event.clientY > bottom)
-			){
-			element.style.position = 'relative'
-			element.style.left = container.style.left
-			element.style.top = container.style.top
-		}
+	// 	if(!containerElement.style.position){
+	// 		containerElement.style.position = 'relative'
+	// 		containerElement.style.left = 0
+	// 		containerElement.style.top = 0
+	// 	}
+
+	// 	var { left, right, top, bottom } = getCoordinates(containerElement)
+
+	// 	if((event.clientX < left || event.clientX > right)
+	// 			|| (event.clientY < top || event.clientY > bottom)
+	// 		){
+	// 		this.currentElement.style.position = 'relative'
+	// 		this.currentElement.style.left = containerElement.style.left
+	// 		this.currentElement.style.top = containerElement.style.top
+	// 	}
 
 		
-	}
+	// }
 
-	function getCoordinates(containerDOM){
-		return containerDOM.getBoundingClientRect()
-	}
+	// function getCoordinates(containerDOM){
+	// 	return containerDOM.getBoundingClientRect()
+	// }
 }
